ae1eb6b2ab24dfbaee4cf4a2e7824c7d0e03a39b
Sean Allred <code@seanallred.com>
2015-02-01 01:27:44 -0600

    Add some intro documentation

--- stash.el
+++ stash.el
@@ -32,6 +32,20 @@
 ;; variables are written to disk.  Where no app is given, the
 ;; `stash-default-application' is used, set to save every minute.
 
+;; To get started, define an application using `defapp':
+;;
+;;   (defapp my-app 120)
+;;
+;; The above defines my-app to save its data every two minutes (and
+;; when Emacs is killed, of course).
+;;
+;; You can now define variables similarly as you would with `defvar',
+;; but now you must provide a filename to save to (under
+;; `stash-directory' and the applications's associated subdirectory):
+;;
+;;   (defstash my-var "var.el" my-app 'spam
+;;     "This docstring is marginally useful.")
+
 ;;; Code:
 
 
